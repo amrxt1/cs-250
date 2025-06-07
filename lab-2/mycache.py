e8ea5355a397f6612fc42f5d2d3a9982c40fbf31
amrxt1 <115386946+amrxt1@users.noreply.github.com>
2025-06-06 20:21:15 -0700

    Implement hits and total_time calculation based on where addr found

--- lab-2/mycache.py
+++ lab-2/mycache.py
@@ -13,9 +13,9 @@ class MyCache:
         # assume copying 1 block from L3 to L2 takes l2_latency + l3_latency time;
         # assume copying 1 block from L1 from main memory takes l1_latency +
         #        l2_latency + l3_latency + m_latency time.
-        self.l1 = deque([""] * l1_size, maxlen=l1_size)
-        self.l2 = deque([""] * l2_size, maxlen=l2_size)
-        self.l3 = deque([""] * l3_size, maxlen=l3_size)
+        self.l1 = deque(maxlen=l1_size)
+        self.l2 = deque(maxlen=l2_size)
+        self.l3 = deque(maxlen=l3_size)
         self.l1_late = l1_latency
         self.l2_late = l2_latency
         self.l3_late = l3_latency
@@ -30,21 +30,36 @@ class MyCache:
         # total_time is the total amount of time it takes for all addresses in adds to be accessed
 
         # -----write your programme from here-----
+        hits = 0
 
         for addr in adds:
-            print("looking for ", addr, " in l1")
-            print(addr in self.l1)
-            print("looking for ", addr, " in l2")
-            print(addr in self.l2)
-            print("looking for ", addr, " in l3")
-            print(addr in self.l3)
+            print("Addr: ", addr)
+            if addr in self.l1:
+                print("L1 hit")
+                hits += 1
+                total_time += self.l1_late
 
-        # Default solution:
-        # all accesss has to be from main memory
-        total_time = len(adds) * (
-            self.l1_late + self.l2_late + self.l3_late + self.m_late
-        )
-        hit_rate = 0 / len(adds)
+            elif addr in self.l2:
+                print("L1 miss")
+                print("L2 hit")
+
+                total_time += self.l1_late + self.l2_late
+
+            elif addr in self.l3:
+                print("L1 miss")
+                print("L2 miss")
+                print("L3 hit")
+
+                total_time += self.l1_late + self.l2_late + self.l3_late
+
+            else:
+                print("L1 miss")
+                print("L2 miss")
+                print("L3 miss")
+
+                total_time += self.l1_late + self.l2_late + self.l3_late + self.m_late
+
+        hit_rate = hits / len(adds)
 
         # -----do not change the output-----
         return total_time, hit_rate
